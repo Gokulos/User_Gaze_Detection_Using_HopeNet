ed20aeca415287e04f199cde3c0447fd5df47375
Gokul O S <gokulos40@gmail.com>
2025-10-05 19:09:01 +0200

    Create preprocessing.py

---
+++ Code/preprocessing.py
@@ -0,0 +1,16 @@
+import cv2
+from torchvision import transforms
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+preprocess = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+def detect_face(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces[0] if len(faces) > 0 else None
